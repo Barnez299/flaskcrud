60d132de797bfd2cd049f102c04a2a316dbf4224
barnez299 <barnez29@gmail.com>
2020-09-14 17:31:02 +0200

    updated with database variables

--- app.py
+++ app.py
@@ -4,6 +4,36 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+
+
+ENV = 'dev'
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456789@localhost/employees'
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Employee(db.Model):
+    __tablename__ = 'employees'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200))
+    email = db.Column(db.String(200), unique=True)
+    phone = db.Column(db.Integer)
+
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+    
+
+
 @app.route('/')
 def Index():
     return render_template('index.html')
@@ -11,4 +41,4 @@ def Index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
