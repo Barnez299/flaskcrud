ed5e8a054ed4904b6c93cf4b372402f509c2056f
barnez299 <barnez29@gmail.com>
2020-09-15 12:50:47 +0200

    updated with database test values stored in db

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import psycopg2
+
 
 
 app = Flask(__name__)
@@ -25,7 +28,7 @@ class Employee(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200))
     email = db.Column(db.String(200), unique=True)
-    phone = db.Column(db.Integer)
+    phone = db.Column(db.String(10))
 
     def __init__(self, name, email, phone):
         self.name = name
@@ -34,9 +37,28 @@ class Employee(db.Model):
     
 
 
+
 @app.route('/')
 def Index():
     return render_template('index.html')
+
+@app.route('/insert', methods = ['POST'])
+def insert():
+ 
+    if request.method == 'POST':
+ 
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+ 
+ 
+        data = Employee(name, email, phone)
+        db.session.add(data)
+        db.session.commit()
+
+        return redirect(url_for('Index'))
+
+        
 
 
 
